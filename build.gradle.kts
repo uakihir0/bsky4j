9a8af8b3b11d2c6c5a5e868552fbaa49c2ccdc58
Akihiro Urushiara <a.urusihara@gmail.com>
2023-04-22 14:50:32 +0900

    update grradle version.

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    // Apply the java-library plugin for API and implementation separation.
+    `java-library`
+    `maven-publish`
+}
+
+group = "bsky4j"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven(uri("https://jitpack.io"))
+}
+
+dependencies {
+
+    // J2ObjC
+    implementation("com.github.uakihir0:JLogger:1.4")
+    implementation("com.github.uakihir0:JHttpClient:1.1.9")
+    implementation("com.google.code.findbugs:jsr305:3.0.2")
+    // Library
+    implementation("com.google.code.gson:gson:2.8.2")
+
+    // Test
+    testImplementation("junit:junit:4.13.2")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
